96b1cb7b42e7753d8cd5331eef5e3342875d50f2
supraof <kolchevskiy2@gmail.com>
2021-11-04 13:22:18 +0200

    get movies - api

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
+
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
 
-  add = () => {
-    this.setState(current => ({count: current.count + 1}))
-  };
-
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}))
+  getMovies = async () => {
+    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
   };
 
   componentDidMount() {
-    console.log('Компонент отрендерился');
-  }
-
-  componentDidUpdate() {
-    console.log('Компонент обновился');
-  }
-
-  componentWillMount() {
-    console.log('Компонент умер :C');
+    this.getMovies();
   }
 
   render() {
-    console.log('Я рендерюсь');
-     return <div>
-       <h1>Текущее число: {this.state.count}</h1>
-       <button onClick={this.add}>Плюс</button>
-       <button onClick={this.minus}>Минус</button>
-     </div>
+    const {isLoading} = this.state;
+    return <div>{this.state.isLoading ? "Загрузка" : "Страница загружена"}</div>
   }
 }
 
